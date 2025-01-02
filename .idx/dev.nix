db68be1d68c9bee1b6b993c2cf8aa6501f5a595c
magnolia.gcloud <magnolia.gcloud@gmail.com>
2025-01-02 08:29:35 +0000

    add project dependencies

---
+++ .idx/dev.nix
@@ -0,0 +1,28 @@
+{pkgs}: {
+  channel = "stable-24.05";
+  packages = [
+    pkgs.nodejs_20
+  ];
+  idx.extensions = [
+    "angular.ng-template"
+    "bradlc.vscode-tailwindcss"
+  ];
+  idx.previews = {
+    previews = {
+      web = {
+        command = [
+          "npm"
+          "run"
+          "start"
+          "--"
+          "--port"
+          "$PORT"
+          "--host"
+          "0.0.0.0"
+          "--disable-host-check"
+        ];
+        manager = "web";
+      };
+    };
+  };
+}
